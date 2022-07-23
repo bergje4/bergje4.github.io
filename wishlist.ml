9818eb218bd8ee5787b3d25811efbf84ce1b5805
bergje4 <bergje-github@bergje.nl>
2022-07-23 09:54:10 +0000

    Create wishlist.ml

---
+++ wishlist.ml
@@ -0,0 +1,18 @@
+---
+layout: page
+title: Verlanglijstje
+permalink: /verlanglijst
+---
+
+# Sport
+
+## Calisthenics
+
+* [Push-Up Bars With Ergonomical Wooden Handle](https://www.pullup-dip.com/push-up-bars)
+* [High Quality Wooden Parallettes With Ergonomic Wooden Handle - Medium Version](https://www.pullup-dip.com/wooden-parallettes?number=P-00501)
+
+## Klimmen
+
+* Moon Climbing Moon Dust [link](https://www.bergfreunde.nl/moon-climbing-moon-dust-magnesium/), [link](https://www.klimwinkel.nl/moon-climbing-moon-dust) []()
+* [Lattice - Triple Rung - Trainingsbord](https://www.bergfreunde.nl/lattice-triple-rung-trainingsbord/)
+
